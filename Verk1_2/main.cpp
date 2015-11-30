bf8d98dc17dbec7216ae4e46f8098c0e3040e3e2
carlosfridrik <carlosfridrik1988@gmail.com>
2015-11-30 13:04:23 +0000

    main loop

--- Verk1_2/main.cpp
+++ Verk1_2/main.cpp
@@ -1,30 +1,20 @@
 #include <QCoreApplication>
 #include <iostream>
 #include <person.h>
-<<<<<<< HEAD
 #include <replayer.h>
-=======
 #include <datalayer.h>
->>>>>>> fa8204fbbc0061e7caea5002ee057d0695c865fe
 using namespace std;
 
-
-list<Person> personList;
-
-
-// Should probably be in the domain layer.
-void addPerson(Person newPerson)
-{
-    personList.push_back(newPerson);
-}
+vector<Person> personList;
 
 int main(int argc, char *argv[])
 {
+    RepLayer userInt(personList);
+    while (true)
+    {
+        userInt.startScreen();
+    }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> fa8204fbbc0061e7caea5002ee057d0695c865fe
     QCoreApplication a(argc, argv);
     return 0;
 }
