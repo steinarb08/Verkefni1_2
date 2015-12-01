014a4833bd78d1c64525e10782351c7f40d0083f
steinarb08 <steinarbjornsson@hotmail.com>
2015-12-01 13:27:29 +0000

    Commented the Person class

--- Verk1_2/person.cpp
+++ Verk1_2/person.cpp
@@ -1,10 +1,12 @@
 #include "person.h"
 
+// Default constructor
 Person::Person()
 {
 
 }
 
+// Constructor that sets name,gender,birthyear and deathyear to input value.
 Person::Person(string _name,string _gender,int _birthyear,int _deathyear)
 {
     name = _name;
@@ -14,42 +16,49 @@ Person::Person(string _name,string _gender,int _birthyear,int _deathyear)
 }
 
 
-
+// Returns the name of a Person instance
 string Person::getName()
 {
     return name;
 }
 
+// Returns the gender of a Person instance
 string Person::getGender()
 {
     return gender;
 }
 
+// Returns the year of birth of a Person instance
 int Person::getBirthYear()
 {
     return birthYear;
 }
 
+//Returns the year of death of a Person instance
 int Person::getDeathYear()
 {
     return deathYear;
 }
 
+// Sets the name of a Person instance to input value
 void Person::setName(string _name)
 {
     name = _name;
 }
 
+// Sets the gender of a Person instance to input value
 void Person::setGender(string _gender)
 {
     gender = _gender;
 }
 
+// Sets the year of birth of a Person instance to input value
 void Person::setBirthYear(int _birthYear)
 {
     birthYear = _birthYear;
 }
 
+// Sets the year of death of a Person instance to input value
 void Person::setDeathYear(int _deathYear)
 {
     deathYear = _deathYear;
--- Verk1_2/person.h
+++ Verk1_2/person.h
@@ -7,17 +7,37 @@ class Person
 {
 public:
     Person();
+    // Default constructor
+
     Person(string _name,string _gender,int _birthyear,int _deathyear);
+    // Constructor that sets name,gender,birthyear and deathyear to input value.
+
     string getName();
+    // Returns the name of a Person instance
+
     string getGender();
+    // Returns the gender of a Person instance
+
     int getBirthYear();
+    // Returns the year of birth of a Person instance
+
     int getDeathYear();
+    //Returns the year of death of a Person instance
+
     void setName(string _name);
+    // Sets the name of a Person instance to input value
+
     void setGender(string _gender);
+    // Sets the gender of a Person instance to input value
+
     void setBirthYear(int _birthYear);
+    // Sets the year of birth of a Person instance to input value
+
     void setDeathYear(int _deathYear);
+    // Sets the year of death of a Person instance to input value
 
 private:
+    // Name,gender,year of birth and year of death of a Person instance. Accessed by get/set functions.
     string name;
     string gender;
     int birthYear;
