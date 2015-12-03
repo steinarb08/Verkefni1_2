bddebefceb03bc9ad4c1891e4d40ccc6f385c631
arnarh15@ru.is <arnarh15@ru.is>
2015-12-03 13:07:04 +0000

    Small changes to main.cpp

--- Verk1_2/main.cpp
+++ Verk1_2/main.cpp
@@ -6,6 +6,7 @@
 #include <replayer.h>
 #include <datalayer.h>
 #include <domainlayer.h>
+#include <computer.h>
 using namespace std;
 vector<Person> personList;
 int main(int argc, char *argv[])
@@ -19,7 +20,7 @@ int main(int argc, char *argv[])
    // Run the welcome screen until user decides to quit.
    while(true)
    {
-       rep1.startScreen();
+       rep1.firstStartScreen();
    }
     QCoreApplication a(argc, argv);
     return 0;
