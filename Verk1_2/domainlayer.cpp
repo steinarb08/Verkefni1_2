dd1b04ea98f354d1f506c15fe6c92aa4be09b8bf
carlosfridrik <carlosfridrik1988@gmail.com>
2015-12-04 13:44:26 +0000

    changed 2 sort functions in domain

--- Verk1_2/domainlayer.cpp
+++ Verk1_2/domainlayer.cpp
@@ -6,22 +6,8 @@ DomainLayer::DomainLayer()
 }
 
 vector<Person> DomainLayer::sortFromAtoZ(vector<Person> personList){            //Sorts the names in alphabetical order (A-Z) using bubble sort.
-    bool swapped;
-    do
-    {
-        swapped = false;
-        for(unsigned int i = 1; i < personList.size(); i++)
-        {
-            //Swapping elements in if statement.
-            if(personList.at(i-1).getName() > personList.at(i).getName())
-            {
-                swap(personList.at(i-1), personList.at(i));
-                swapped = true;
-            }
-        }
-    } while(swapped);
-
-    return personList;
+    DataLayer d1;
+    return d1.loadDbPersonSort("name",1);
 }
 
 void DomainLayer::swap(Person& per1, Person& per2){
@@ -31,10 +17,8 @@ void DomainLayer::swap(Person& per1, Person& per2){
 }
 
 vector<Person> DomainLayer::reverse(vector<Person> personList){             //Sorts the names in reverse alphabetical order (Z-A) using bubble sort.
-    personList = sortFromAtoZ(personList);
-    std::reverse(personList.begin(), personList.end());
-
-    return personList;
+    DataLayer d1;
+    return d1.loadDbPersonSort("name",0);
 }
 
 vector<Person> DomainLayer::sortGender(vector<Person> personList){          //Sorts the list by gender in alphabetical order using bubble sort.
