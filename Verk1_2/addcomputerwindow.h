9b967741121777581ff4bd262e0e38c9d6c5bd77
olafurv15 <olafurv15@ru.is>
2015-12-15 15:24:18 +0000

    Added comments to addcomputerwindow.h file

--- Verk1_2/addcomputerwindow.h
+++ Verk1_2/addcomputerwindow.h
@@ -15,19 +15,25 @@ class AddComputerWindow : public QMainWindow
 
 public:
      AddComputerWindow(QWidget *parent,DomainLayer dom, ComputerScreen *cpuScreen);
+     // Default constructor
     ~AddComputerWindow();
 
 private slots:
     void on_checkYes_clicked();
+    // Only one box can be checked.
 
     void on_checkNo_clicked();
+    // Only one box can be checked.
 
     void on_btnAddC_clicked();
+    // Adds new computer to database.
 
     void on_btnCancelC_2_clicked();
+    // Closes the window when entered "Cancel" box
 
 private:
     DomainLayer d1;
+    // includes functions from datalayer
     Ui::AddComputerWindow *ui;
     ComputerScreen *cs1;
 
