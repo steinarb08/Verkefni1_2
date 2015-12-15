bbe2eb6802c43695805d58c7a46ae2e848b52014
olafurv15 <olafurv15@ru.is>
2015-12-15 15:01:47 +0000

    Added comments to addnewperson cpp and h file

--- Verk1_2/addnewperson.h
+++ Verk1_2/addnewperson.h
@@ -19,20 +19,25 @@ class addNewPerson : public QDialog
 
 public:
      addNewPerson(QWidget *parent,DomainLayer &dom, PersonScreen *perScreen);
+     // Default constructor
     ~addNewPerson();
 
 private slots:
     void on_Button_Cancel_NewScientist_clicked();
 
     void on_Button_ADD_NewScientist_clicked();
+    // Adds scientist to data base.
 
     void on_checkBox_male_clicked();
+    // Only one box is allowed to be checked
 
     void on_checkBox_female_clicked();
+    // Only one box is allowed to be checked
 
 private:
     Ui::addNewPerson *ui;
     DomainLayer d1;
+    // functions in domainlayer are included
     PersonScreen *ps1;
 };
 
