37555e9d39babff120c64191ff896ad6aa2fd634
olafurv15 <olafurv15@ru.is>
2015-12-15 20:46:06 +0000

    Added comments in computerscreen h and cpp file

--- Verk1_2/computerscreen.cpp
+++ Verk1_2/computerscreen.cpp
@@ -4,7 +4,7 @@
 #include "addcomputerwindow.h"
 #include "QMessageBox"
 
-
+// Default constructor. Loads list.
 ComputerScreen::ComputerScreen(QWidget *parent,DomainLayer &dom) :
     QMainWindow(parent),
     ui(new Ui::ComputerScreen)
@@ -14,7 +14,6 @@ ComputerScreen::ComputerScreen(QWidget *parent,DomainLayer &dom) :
     computerList = d1.loadComputer();
     updateComputers();
     on_sortComputerC_currentTextChanged();
-
 }
 
 ComputerScreen::~ComputerScreen()
@@ -22,76 +21,75 @@ ComputerScreen::~ComputerScreen()
     delete ui;
 }
 
+// Add computer button.
 void ComputerScreen::on_pushButton_clicked()
 {
     AddComputerWindow *addcomputerwindow = new AddComputerWindow(this,d1,this);
     addcomputerwindow->show();
 }
 
+// Updates computer list function.
 void ComputerScreen::updateComputers()
 {
     ui->listComputer->clear();
     computerList = d1.loadComputer();
-        for(unsigned int i=0;i<computerList.size();i++)
-        {
-            ui->listComputer->addItem(QString::fromStdString(computerList.at(i).getName()));
-        }
-
+    for(unsigned int i=0;i<computerList.size();i++)
+    {
+        ui->listComputer->addItem(QString::fromStdString(computerList.at(i).getName()));
+    }
 }
 
+// Shows information about the selected computer.
 void ComputerScreen::on_listComputer_clicked()
 {
     int i = ui->listComputer->currentRow();
     ui-> btnRemoveC-> setEnabled(true);
     ui->btnEditC->setEnabled(true);
 
-     ui->lblComputerName->setText(QString::fromStdString(computerList.at(i).getName()));
-     ui->lblBuiltYear->setText(QString::number(computerList.at(i).getBuiltYear()));
-     ui->lblType->setText(QString::fromStdString(computerList.at(i).getType()));
-     ui->lblBuilt->setText(QString::fromStdString(computerList.at(i).getBuiltComputer()));
+    ui->lblComputerName->setText(QString::fromStdString(computerList.at(i).getName()));
+    ui->lblBuiltYear->setText(QString::number(computerList.at(i).getBuiltYear()));
+    ui->lblType->setText(QString::fromStdString(computerList.at(i).getType()));
+    ui->lblBuilt->setText(QString::fromStdString(computerList.at(i).getBuiltComputer()));
 }
 
+// Remove button. Removes selected computer from data base.
 void ComputerScreen::on_btnRemoveC_clicked()
 {
     if(ui->listComputer->selectedItems().empty())
     {
         QMessageBox::information(NULL,"Remove error","No item selected!");
     }
-
     else
     {
-            int answer = QMessageBox::question(this, "Confirm", "Are you sure?");
-            if(answer == QMessageBox::No)
-            {
-                return;
-            }
+        int answer = QMessageBox::question(this, "Confirm", "Are you sure?");
+        if(answer == QMessageBox::No)
+        {
+            return;
+        }
 
-            int selectedComputerIndex = ui->listComputer->currentRow();
-            d1.deleteFromComputer(computerList.at(selectedComputerIndex));
-            ui->listComputer->clear();
-            computerList = d1.loadComputer();
-            updateComputers();
+        int selectedComputerIndex = ui->listComputer->currentRow();
+        d1.deleteFromComputer(computerList.at(selectedComputerIndex));
+        ui->listComputer->clear();
+        computerList = d1.loadComputer();
+        updateComputers();
     }
-
-
-
 }
 
+// Edit button. Edit selected computer.
 void ComputerScreen::on_btnEditC_clicked()
 {
     if(ui->listComputer->selectedItems().empty())
     {
         QMessageBox::information(NULL,"Edit error","No item selected!");
     }
-
     else
     {
         EditComputer *editcomputer = new EditComputer(this, d1,computerList.at(ui->listComputer->currentRow()), this);
         editcomputer->show();
     }
-
 }
 
+// Searches for a substring (searchString) in the name of each Computer in a list, returns a sublist that has those substrings.
 void ComputerScreen::on_textBoxSearchComp_textChanged()
 {
     ui-> listComputer-> clear();
@@ -105,9 +103,12 @@ void ComputerScreen::on_textBoxSearchComp_textChanged()
     }
 }
 
+// Sort options.
 void ComputerScreen::on_sortComputerC_currentTextChanged()
 {
     string sort = ui->sortComputerC->currentText().toStdString();
+
+    // Sorts the names in alphabetical order (A-Z) or descending alphabetical order(Z-A).
     if(sort == "Name")
     {
         if(ui->checkDescendingC->isChecked())
@@ -119,7 +120,7 @@ void ComputerScreen::on_sortComputerC_currentTextChanged()
             computerList = d1.sortFromAtoZ_C();
         }
     }
-
+    // Sorts types in ascending or descending order
     else if(sort == "Type")
     {
         if(ui->checkDescendingC->isChecked())
@@ -131,7 +132,7 @@ void ComputerScreen::on_sortComputerC_currentTextChanged()
             computerList = d1.sortType_C();
         }
     }
-
+    // Sorts build year in ascending or descending order
     else if(sort == "Build Year")
     {
         if(ui->checkDescendingC->isChecked())
@@ -143,7 +144,7 @@ void ComputerScreen::on_sortComputerC_currentTextChanged()
             computerList = d1.sortYearBuild_C();
         }
     }
-
+    // Sorts whether the computer was built or not in ascending or descending order.
     else if(sort == "Was It Built?")
     {
         if(ui->checkDescendingC->isChecked())
@@ -156,16 +157,15 @@ void ComputerScreen::on_sortComputerC_currentTextChanged()
         }
     }
 
+    // updates order list.
     ui->listComputer->clear();
-
     for(unsigned int i=0;i<computerList.size();i++)
     {
         ui->listComputer->addItem(QString::fromStdString(computerList.at(i).getName()));
     }
-
 }
 
-
+// Descending order check box.
 void ComputerScreen::on_checkDescendingC_clicked()
 {
     on_sortComputerC_currentTextChanged();
--- Verk1_2/computerscreen.h
+++ Verk1_2/computerscreen.h
@@ -19,25 +19,33 @@ class ComputerScreen : public QMainWindow
 
 public:
     ComputerScreen(QWidget *parent,DomainLayer &dom);
+    // Default constructor. Loads list.
     ~ComputerScreen();
 
     void updateComputers();
+    // Updates computer list function.
 
 private slots:
     void on_pushButton_clicked();
+    // Add computer button.
 
     void on_listComputer_clicked();
+    // Shows information about the selected computer.
 
     void on_btnRemoveC_clicked();
+    // Remove button. Removes selected computer from data base.
 
     void on_btnEditC_clicked();
+    // Edit button. Edit selected computer.
 
     void on_textBoxSearchComp_textChanged();
-
+    // Searches for a substring (searchString) in the name of each Computer in a list, returns a sublist that has those substrings.
 
     void on_sortComputerC_currentTextChanged();
+    // Sort options.
 
     void on_checkDescendingC_clicked();
+    // Descending order check box.
 
 private:
     DomainLayer d1;
